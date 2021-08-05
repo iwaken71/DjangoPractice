a8d084d39b5b77b3effca4afa82dd50bf727ea49
iwaken71 <iwaken59371518@gmail.com>
2021-08-05 16:40:55 +0900

    html test

--- snippets/views.py
+++ snippets/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpRequest
+from snippets.models import Snippet
 
 
 def top(request):
-    return render(request, "snippets/top.html")
+    snippets = Snippet.objects.all()
+    context = {"snippets": snippets}
+    return render(request, "snippets/top.html", context)
 
 
 def snippet_new(request):
