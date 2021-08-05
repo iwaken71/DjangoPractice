03826a3db5e1cf95729df1f502b84a39a1727740
iwaken71 <iwaken59371518@gmail.com>
2021-08-05 16:13:23 +0900

    edit template html

--- snippets/tests.py
+++ snippets/tests.py
@@ -2,35 +2,36 @@ from django.test import TestCase
 
 from django.http import HttpRequest, request, response
 from django.test import TestCase
-from snippets.views import top,snippet_new,snippet_edit,snippet_detail
+from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 from django.urls import resolve
 
-class TopPageViewTest(TestCase):
-    def test_top_returns_200(self):
-        request = HttpRequest()
-        response = self.client.get("/")
-        self.assertEqual(response.status_code,200) 
 
-    def test_top_retuens_expected_content(self):
-        request = HttpRequest()
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_expected_title(self):
         response = self.client.get("/")
-        self.assertEqual(response.content,b"Hello World")
+        self.assertContains(response, "Djangoスニペット", status_code=200)
+
+    def test_top_page_uses_expected_template(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "snippets/top.html")
+
 
 class CreateSnippetTest(TestCase):
     def test_should_resolve_snippet_new(self):
         found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new,found.func)
+        self.assertEqual(snippet_new, found.func)
+
 
 class SnippetDetailTest(TestCase):
     def test_should_resolve_snippet_detail(self):
         found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail,found.func)
+        self.assertEqual(snippet_detail, found.func)
+
+
 class EditSnippetTest(TestCase):
     def test_should_resolve_snippet_edit(self):
         found = resolve("/snippets/1/edit/")
-        self.assertEqual(snippet_edit,found.func)
-        
-
+        self.assertEqual(snippet_edit, found.func)
 
 
 # Create your tests here.
--- snippets/views.py
+++ snippets/views.py
@@ -4,15 +4,19 @@ from django.http import HttpRequest
 
 
 def top(request):
-    return HttpResponse(b"Hello World")
+    return render(request, "snippets/top.html")
+
+
 def snippet_new(request):
     return HttpResponse('スニペットの登録')
+
+
 def snippet_edit(request):
     return HttpResponse('スニペットの編集')
-def snippet_detail(request,snippet_id):
+
+
+def snippet_detail(request, snippet_id):
     return HttpResponse('スニペットの詳細閲覧')
-
-
 
 
 # Create your views here.
